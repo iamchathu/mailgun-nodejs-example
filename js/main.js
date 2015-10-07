130b9d58d8309b668f347bcdb06880e55e133276
Chathu Vishwajith <4evercreations@gmail.com>
2015-10-07 13:20:26 +0530

    Fix JS bug

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 var vForm = document.getElementById('mgform');
 var vInput = document.getElementById('mail');
-
+var vBody = document.getElementById('mailBody');
+var location;
 vForm.onsubmit = function() {
 	if (this.hid == "bulk") {
-		location = "/submit/" + encodeURIComponent(vInput.value);
+		location = "/submit/" + encodeURIComponent(vInput.value)+"/"+encodeURIComponent(vBody.value);
 	}
 	if (this.hid == "list") {
 		location = "/validate/" + encodeURIComponent(vInput.value);
 	}
 	if (this.hid == "inv") {
-		location = "/invoice/" + encodeURIComponent(vInput.value);
+		location = "/invoice/" + encodeURIComponent(vInput.value)+"/"+encodeURIComponent(vBody.value);
 	}
 
 	return false;
